5de5a5bbd22ca3c5aefbe459fe385791cea22f74
EC2 Default User <ec2-user@ip-172-31-76-158.ec2.internal>
2022-10-10 09:16:59 +0000

    having protoc version error

--- training/install-nvidia-runtime.sh
+++ training/install-nvidia-runtime.sh
@@ -4,8 +4,11 @@
 # The ndivia runtime will then be available to the containers.
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+#curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo yum add -
+#curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/yum/sources.list.d/nvidia-docker.list
 
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+#sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo yum update && sudo yum install -y nvidia-container-toolkit
 sudo systemctl restart docker
